f9ad641dcd07b9d15799b3db0b6135e43786ab99
Mirjam Uher <uher.mirjam@gmail.com>
2021-01-31 14:49:32 +1100

    initial setup of checkin process

--- project/eventmanager/urls.py
+++ project/eventmanager/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path
-from . import views
-import views.create_event
-import views.landing_page
-import views.manage_event
-import views.edit_participant
-import views.checkin
+from eventmanager.views import create_event, landing_page, manage_event, edit_participant, checkin
 
 app_name = 'eventmanager'
 urlpatterns = [
-    path('', views.landing_page.landing_page, name='landing_page'),
-    path('create', views.create_event.create_event, name='create_event'),
-    path('manage/<int:event_id>', views.manage_event.manage_event, name='manage_event'),
+    path('', landing_page.landing_page, name='landing_page'),
+    path('create', create_event.create_event, name='create_event'),
+    path('manage/<int:event_id>', manage_event.manage_event, name='manage_event'),
     # path('manage/<int:event_id>/<int:participant_id>', views.manage_participants, name='manage_participants'),
-    path('manage/participant/<int:participant_id>', views.edit_participant.edit_participant, name='edit_participant'),
-    path('checkin/<int:event_id>/<int:participant_id>', views.checkin.checkin, name='checkin'),
+    path('manage/participant/<int:participant_id>', edit_participant.edit_participant, name='edit_participant'),
+    path('checkin/<int:event_id>/<int:participant_id>', checkin.checkin, name='checkin'),
 ]
--- project/eventmanager/views/checkin.py
+++ project/eventmanager/views/checkin.py
@@ -4,7 +4,31 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.forms import ModelForm
 from eventmanager.models import Event, Participant, Registration
+from datetime import datetime
+
+def registration(event, participant):
+    try:
+        rego = Registration.objects.filter(participant=participant, event=event).get()
+        if not rego.checked_in_at:
+            now = datetime.now()
+            rego.checked_in_at = now
+            rego.full_clean()
+            rego.save()
+            return f"Success: Registration for {participant.first_name} {participant.last_name} for {event.name} was successful"
+        else:
+            return f"{participant.first_name} {participant.last_name} already checked in for {event.name} on {rego.checked_in_at}"
+    except Registration.DoesNotExist:
+        return "Error: Participant is not registered for this event"
 
 def checkin(request, event_id, participant_id):
-    response = f"You have checked in {participant_id}"
-    return HttpResponse(response)
+    event = get_object_or_404(Event, pk=event_id)
+    participant = get_object_or_404(Participant, pk=participant_id)
+
+    msg = registration(event, participant)
+
+    context = {
+        'event': event,
+        'p': participant,
+        'msg': msg
+    }
+    return render(request, 'eventmanager/checkin.html', context)
