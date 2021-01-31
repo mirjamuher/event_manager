52b1b73c02b6534c438a5ea61c952652d4845e4c
Mirjam Uher <uher.mirjam@gmail.com>
2021-01-31 11:00:32 +1100

    initial handling of csv files of participants

--- project/eventmanager/models/participant.py
+++ project/eventmanager/models/participant.py
@@ -5,9 +5,9 @@ from django.db import models
 class Participant(models.Model):
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
-    organisation_name = models.CharField(max_length=100, blank=True)
+    organisation = models.CharField(max_length=100, blank=True)
     position = models.CharField(max_length = 100, blank=True)
-    email = models.EmailField(max_length=250, blank=True)
+    email = models.EmailField(max_length=250, unique=True)
     phone_number = models.CharField(max_length=15, blank=True)
     comment = models.TextField(blank=True)
 
--- project/eventmanager/views.py
+++ project/eventmanager/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django import forms
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.template import loader
 from django.forms import ModelForm
-from eventmanager.models import Event
+from eventmanager.models import Event, Participant, Registration
+import csv
+import io
 
 
 # Create your views here.
@@ -30,11 +33,56 @@ def create_event(request):
         form = EventForm()
     return render(request, 'eventmanager/create_event.html', {'form': form})
 
+def handle_participant_csv_upload(event, djangoStyleFile):
+    uploaded_file_as_bytes = djangoStyleFile.read()
+    uploaded_file_as_a_string = uploaded_file_as_bytes.decode(djangoStyleFile.charset or 'utf-8')
+    file = io.StringIO(uploaded_file_as_a_string)
+    reader = csv.DictReader(file)
+    for row in reader:
+        #TODO: VALIDATE ROW INFO
+        participant = get_or_create_participant(row)
+        get_or_create_registration(participant, event)
+        print(row)
+
+def get_or_create_participant(data:dict):
+    email = data['email'].lower()
+    try: 
+        p = Participant.objects.filter(email=email).get()
+        # TODO: How do I update fields of P that haven't been filled before
+        # without going through each manually?
+    except Participant.DoesNotExist:
+        p = Participant(**data)
+        p.full_clean()
+        p.save()
+    return p
+
+def get_or_create_registration(participant, event):
+    try:
+        rego = participant.registration_set.filter(event=event).get()
+    except Registration.DoesNotExist:
+        rego = Registration(participant=participant, event=event)
+        rego.full_clean()
+        rego.save()
+    return
+
+def manage_event(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    class UploadFileForm(forms.Form):
+        file = forms.FileField()
+
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_participant_csv_upload(event, request.FILES['file'])
+            print(request.FILES)
+            return HttpResponse('Upload was successful')
+    else:
+        form = UploadFileForm()
+    return render(request, 'eventmanager/manage_event.html', {'event':event, 'form':form})
+
+
 
 # Still needs EVERYTHING DONE (even create .html page)
-def manage_event(request, event_id):
-    response = f"You are looking at the event {event_id}"
-    return HttpResponse(response)
 
 def manage_participants(request, event_id):
     response = f"You are managing the participants of event {event_id}"
